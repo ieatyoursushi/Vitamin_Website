f65b783ff7e03552baccb9457a770bc7432f763b
ieatyoursushi <69708951+ieatyoursushi@users.noreply.github.com>
2022-09-09 20:41:06 -0700

    Add project files.

---
+++ Vitamin Website/javascript.js
@@ -0,0 +1,12 @@
+﻿let splitBox = document.querySelector(".splitBox");
+splitBox.children[2].children[0].style.background = "#006AF6";
+splitBox.children[2].children[1].style.color = "black";
+let webMain = document.querySelector(".webMain");
+let login = document.querySelector(".login");
+function toggleDisplay() {
+    webMain.classList.toggle("invisible");
+    login.classList.toggle("invisible");
+}
+//login division
+let submit = document.querySelector(".submit");
+submit.addEventListener('click', toggleDisplay);
