cd79a48d89b4fc29451d5217e1eda1c6696696f7
ieatyoursushi <69708951+ieatyoursushi@users.noreply.github.com>
2022-09-16 18:31:12 -0700

    slides demo completed

--- Vitamin Website/javascript.js
+++ Vitamin Website/javascript.js
@@ -22,9 +22,9 @@ username.addEventListener('change', displayUsername);
 //jumbotron slideshow
 const image1 = "'vitamineimage.PNG'";
 const image2 = "'vitamins stock image 640x427.jpeg'";
-const images = [image2, image1];
+const images = [image2, image1, 'https://img.freepik.com/free-vector/vitamin-food-infographic_23-2148485173.jpg?w=360','https://images.theconversation.com/files/156949/original/image-20170215-19241-1ynym4v.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop'];
 let slideDiv = document.getElementById("splitImage");
-//for UI slide tracker
+//for UI slide tracker, flexible with slides or images array length
 for(let i = 0 ; i < images.length; i++) {
     let circle = document.createElement('div');
     circle.style.borderRadius = "100%";
@@ -32,13 +32,33 @@ for(let i = 0 ; i < images.length; i++) {
     circle.style.height = "10px"
     circle.style.background = "grey";
     circle.classList.add("circle");
+    circle.setAttribute("id", i);
     let slides = document.querySelector(".slides");
     slides.append(circle);
 }
 let circles = document.querySelectorAll(".circle");
-//psuedo recursion
+function grey() {
+    for (let i = 0; i < images.length; i++) {
+        circles[i].style.background = "grey";
+    }
+}
 let i = 0;
+function defaultState() {
+    circles.forEach(circle => {
+        circle.style.background = "grey";
+        circle.addEventListener('click', () => {
+            i = circle.id;
+            slideDiv.style.background = "url(" + images[circle.id] + ")";
+            grey();
+            circles[circle.id].style.background = "white";
+            
+        })
+    })
+}
+//psuedo recursion
+ 
 function cycle() {
+    defaultState();
     slideDiv.style.background = "url(" + images[i] + ")";
     console.log(images[i]);
     circles[i].style.background = "white";
